cb9c33506f8241c079c1297ee4c69b8236b11cb7
MuddledKami <kevinbai0822@163.com>
2018-01-23 21:30:34 +0800

    开始更新

---
+++ src/components/framework-left.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="framework-wrap">
+	  
+  </div>
+</template>
+
+<script>
+export default {
+	name: "toolbar",
+	data() {
+		return {
+			
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.framework-wrap{
+	width: 270px;
+	height: 100vh;
+	background-color: red;
+}
+</style>
