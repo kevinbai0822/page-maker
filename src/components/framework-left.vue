f21ec681da182e29e410a5df1c6a05c7f033b484
MuddledKami <kevinbai0822@163.com>
2018-05-16 22:46:46 +0800

    实现自定义模板功能

--- src/components/framework-left.vue
+++ src/components/framework-left.vue
@@ -1,18 +1,28 @@
 <template>
-  	<div class="wrap">
-		<div class="show-box" :style="{width: toWidth+'px', height: toHeight+'px'}">
-			<div v-for="box in showBox" :key="box.id" :style="box.style"></div>
-		</div>
-		<div class="toolbar">
-			<div class="i-btn" v-for="ti in toolItem" :key="ti.id" @click="boxSplit(ti.arr)">
-				{{ti.item}}
+	<div>
+		<div class="wrap">
+			<div class="show-box" :style="{width: toWidth+'px', height: toHeight+'px'}">
+				<div v-for="box in showBox" :key="box.id" :style="box.style"></div>
+			</div>
+			<div class="toolbar">
+				<div class="i-btn" v-for="ti in toolItem" :key="ti.id" @click="boxSplit(ti.arr)">
+					{{ti.item}}
+				</div>
+			</div>
+			<div class="custom-panel" ref="customPanel">
+				<ul v-for="(cb, cindex) in customBox">
+					<li v-for="(sb, sindex) in cb.subBox" class="cus-box"
+					@click.stop="startCus(sindex, cindex)"
+					@mousemove.stop="movePos(sindex, cindex)"
+					></li>
+				</ul>
+				<div class="mab" v-for="ibp in inBoxPanel"
+				:key="ibp.id"
+				:style="ibp.style"></div>
+				<div class="mab mab-active" :style="inBox" v-if="isMove"></div>
 			</div>
 		</div>
-		<div class="custom-panel" ref="customPanel">
-			<div class="cus-box" v-for="m in 36" :key="m.id" @click="customBox()"></div>
-			<div class="selected-box" v-for="sb in selectedBox" :key="sb.id" :style="sb.style"></div>
-		</div>
-  	</div>
+	</div>
 </template>
 
 <script>
@@ -52,23 +62,90 @@ export default {
 				left: '0'
 			},
 			isMove: false,
-			startPoint:{},
+			startPoint: {},
 			endPoint: {},
 			movePoint: {},
 			customPoint: {},
+			customBox: [
+				{
+					subBox: [
+						{},
+						{},
+						{},
+						{},
+						{},
+						{}
+					]
+				},
+				{
+					subBox: [
+						{},
+						{},
+						{},
+						{},
+						{},
+						{}
+					]
+				},
+				{
+					subBox: [
+						{},
+						{},
+						{},
+						{},
+						{},
+						{}
+					]
+				},
+				{
+					subBox: [
+						{},
+						{},
+						{},
+						{},
+						{},
+						{}
+					]
+				},
+				{
+					subBox: [
+						{},
+						{},
+						{},
+						{},
+						{},
+						{}
+					]
+				},
+				{
+					subBox: [
+						{},
+						{},
+						{},
+						{},
+						{},
+						{}
+					]
+				}
+			],
+			cx: 0,
+			cy: 0,
+			inBoxPanel: [],
+			inBox: {
+				left: 0,
+				top: 0,
+				width: 0,
+				height: 0
+			}
 		}
 	},
 	mounted(){
-		var cus = this.$refs.customPanel
-		this.customPoint.x = cus.offsetLeft
-		this.customPoint.y = cus.offsetTop
-		this.customPoint.width = cus.clientWidth
-		this.customPoint.height = cus.clientHeight
+		document.onmousemove = this.movePoint
 	},
 	methods: {
 		boxSplit(arr){
+			this.showBox.length = 0
 			for(let i=0;i<arr.length;i++){
-				console.log('sss')
 				let boxStyle = {
 					style: {}
 				}
@@ -79,51 +156,71 @@ export default {
 				this.showBox.push(boxStyle)
 			}
 		},
-		customBox(event){
+		startCus(si, ci){
 			this.isMove = !this.isMove
-			if(this.isMove){
-				console.log('move')
-				this.startPoint = this.getMousePos(event)
-				this.selectedBox.push(this.selectedItem)
-				var swid = 192/6
-				var sheg = 192/6
-				var dw = this.startPoint.x - this.customPoint.x
-				var dy = this.startPoint.y - this.customPoint.y
-				this.selectedItem.left = (Math.floor(dw/swid)/6) * 192 + 'px'
-				this.selectedItem.top = (Math.floor(dy/sheg)/6) * 192 + 'px'
-				document.onmousemove = this.movePos
-			}else{
-				this.endPoint = this.getMousePos(event)
+			for(let key in this.startPoint){
+				delete this.startPoint[key]
+			}
+			if(this.isMove === true){
+				this.startPoint.x = ci
+				this.startPoint.y = si
+			}
+			if(this.isMove === false){
+				var temp = {
+					style: {
+						left: null,
+						top: null,
+						width: null,
+						height: null
+					}
+				}
+				for(let i in this.inBox){
+					temp.style[i] = this.inBox[i]
+				}				
+				this.inBoxPanel.push(temp)
 			}
 		},
-		getMousePos(event) {
-			var e = event || window.event;
-			var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
-			var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
-			var x = e.pageX || e.clientX + scrollX;
-			var y = e.pageY || e.clientY + scrollY;
-			return { 'x': x, 'y': y };
-		},
-		movePos(event){
-			this.movePoint = this.getMousePos(event)
-			var diffX = this.movePoint.x - this.startPoint.x
-			var diffY = this.movePoint.y - this.startPoint.y
-			this.selectedItem.width = diffX+'px'
-			this.selectedItem.height = diffY+'px'
+		// getMousePos(event) {
+		// 	var e = event || window.event;
+		// 	var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
+		// 	var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
+		// 	var x = e.pageX || e.clientX + scrollX;
+		// 	var y = e.pageY || e.clientY + scrollY;
+		// 	return { 'x': x, 'y': y };
+		// },
+		movePos(si, ci){
+			if(this.isMove){
+				if(ci > this.startPoint.x){
+					this.movePoint.x = this.startPoint.x
+				}else{
+					this.movePoint.x = ci
+				}
+
+				if(si > this.startPoint.y){
+					this.movePoint.y = this.startPoint.y
+				}else{
+					this.movePoint.y = si
+				}
+				this.inBox.left = this.movePoint.x / this.customBox[0].subBox.length * 192 + 'px'
+				this.inBox.top = this.movePoint.y / this.customBox.length * 192 + 'px'
+				this.inBox.width = (Math.abs(ci - this.startPoint.x) + 1) * 32 + 'px'
+				this.inBox.height = (Math.abs(si - this.startPoint.y) + 1) * 32 + 'px'
+			}
 		}
 	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style lang="scss">
 .wrap {
   display: flex;
   .show-box {
     position: relative;
     border: 1px solid rgb(141, 134, 134);
     div {
-      position: absolute;
+		position: absolute;
+		border: 1px solid #46e832;
     }
   }
   .toolbar {
@@ -147,15 +244,34 @@ export default {
     height: 192px;
     display: flex;
     flex-wrap: wrap;
+	ul{
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		li{
+			pointer-events:auto;
+		}
+	}
     .cus-box{
-      border: 1px solid red;
-      width: 30px;
-      height: 30px;
+		border: 1px solid red;
+		width: 30px;
+		height: 30px;
     }
 	.selected-box{
 		position: absolute;
 		background-color: blue;
 	}
   }
+}
+.mab{
+	position: absolute;
+	border: 1px solid blue;
+	width: 30px;
+	height: 30px;
+	background-color: rgba(0, 0, 205, 0.6);
+	pointer-events: auto;
+}
+.mab-active{
+	pointer-events: none;
 }
 </style>
